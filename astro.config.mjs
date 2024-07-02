c734fe07c7e2549c453dfaf75815e8f59c182701
Adinda Salsabila <149690386+RaionClaire@users.noreply.github.com>
2024-07-02 16:50:17 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    base: 'RaionClaire.github.io',
+    site: 'https://raionclaire.github.io',
 });
